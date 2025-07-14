02cc4791734a235c6b66ad1627b01465d2b5b64d
“Conner” <Connerpaull8@gmail.com>
2025-07-14 00:17:59 -0600

    Saved stable working version after restore

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c3db7cd9dc07bb0"; }
+/******/ 		__webpack_require__.h = function() { return "d6a5320119859f60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/.next/static/webpack/webpack.2cf9a623a40c3074.hot-update.js
+++ app/.next/static/webpack/webpack.2cf9a623a40c3074.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6a171b870f7cff19"; }
+/******/ 	__webpack_require__.h = function() { return "d6a5320119859f60"; }
 /******/ }();
 /******/ 
 /******/ }
